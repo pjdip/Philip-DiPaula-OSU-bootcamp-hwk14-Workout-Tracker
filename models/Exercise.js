3b67e29ff2fd550d4432559efbbd5f717891e552
pjdip <pjdip1@gmail.com>
2021-02-20 20:55:00 -0500

    adding Exercise model

---
+++ models/Exercise.js
@@ -0,0 +1,24 @@
+// Import Mongoose
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Define Exercise Schema
+const ExerciseSchema = new Schema({
+    type: {
+        type: String,
+        trim: true
+    },
+    name: {
+        type: String,
+        trim: true,
+    },
+    weight: Number,
+    sets: Number,
+    reps: Number,
+    duration: Number,
+    distance: Number
+});
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
