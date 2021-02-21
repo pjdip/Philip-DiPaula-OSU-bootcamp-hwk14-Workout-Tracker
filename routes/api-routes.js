cb6e4ffe7d8324f762813d27b82693bf1e22d56b
pjdip <pjdip1@gmail.com>
2021-02-20 22:19:06 -0500

    adding exercise put route to api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,17 +5,24 @@ const router = express.Router();
 // Get Last Workout
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({})
-        .then(workouts => {
-            res.json(workouts);
-        })
+        .then(workouts => res.json(workouts))
         .catch(err => res.json(err));
 });
 
 // Create New Workout
 router.post("/api/workouts", ({ body }, res) => {
     db.Workout.create(body)
-        .then(newWorkout => {
-            res.json(newWorkout);
-        })
+        .then(newWorkout => res.json(newWorkout))
+        .catch(err => res.json(err));
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    db.Exercise.create(body)
+        .then(({ _id }) => db.Workout.findOneAndUpdate(
+            params.id,
+            { $push: { exercises: _id }},
+            { new: true }
+        ))
+        .then(workout => res.json(workout))
         .catch(err => res.json(err));
 });
