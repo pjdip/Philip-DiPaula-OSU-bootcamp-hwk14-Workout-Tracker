7e21c97061304603653f41b70cdaf6c24f75a43f
pjdip <pjdip1@gmail.com>
2021-02-20 22:44:48 -0500

    exercise html route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,4 +7,8 @@ router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
 module.exports = router;
