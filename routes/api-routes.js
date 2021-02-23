e528efe744e65b69eb97213d47c8c02d09251bc6
pjdip <pjdip1@gmail.com>
2021-02-23 14:05:05 -0500

    cleaning up api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,21 +6,41 @@ const router = express.Router();
 // Get Workouts
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({})
-        .then(workouts => {
-            res.json(workouts)
-            console.log(workouts)})
-        .catch(err => res.json(err));
+    .then(workouts => res.json(workouts))
+    .catch(err => res.json(err));
 });
 
 // Create New Workout
 router.post("/api/workouts", ({ body }, res) => {
     db.Workout.create(body)
-        .then(newWorkout => res.json(newWorkout))
-        .catch(err => res.json(err));
+    .then(newWorkout => res.json(newWorkout))
+    .catch(err => res.json(err));
 });
 
 // Add Exercise to Workout with provided id
 router.put("/api/workouts/:id", ({ body, params }, res) => {
+    db.Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body }},
+        { new: true, useFindAndModify: false }
+    )
+    .then(workout => res.json(workout))
+    .catch(err => res.json(err));
+});
+
+// Get Workout Range
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+    .limit(7)
+    .then(workouts => res.json(workouts))
+    .catch(err => res.json(err));
+});
+
+// Export the router
+module.exports = router;
+
+
+//reject put route code
 /*     db.Workout.findOneAndUpdate(
         { _id: params.id },
         { $push: { exercises: body } },
@@ -37,24 +57,3 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
             )
             .populate("exercises");
         }) */
-        db.Workout.findByIdAndUpdate(
-            params.id,
-            { $push: { exercises: body }},
-            { new: true, useFindAndModify: false }
-        )
-        .then(workout => {
-            console.log("workout: " + workout);
-            res.json(workout)})
-        .catch(err => res.json(err));
-});
-
-// Get Workout Range
-router.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({})
-        .limit(7)
-        .then(workouts => res.json(workouts))
-        .catch(err => res.json(err));
-});
-
-// Export the router
-module.exports = router;
