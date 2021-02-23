8e24a0c99f0cda17760a50d240a835db006d4edd
pjdip <pjdip1@gmail.com>
2021-02-23 14:01:41 -0500

    adding exercise schema to workout schema directly, adding virtual for totalDuration

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 https://arunrajeevan.medium.com/mongodb-schema-using-mongoose-19aeffdf772f
 https://bezkoder.com/mongoose-one-to-many-relationship/
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+https://stackoverflow.com/questions/29681715/virtuals-vs-methods-in-mongoose
+
--- models/Workout.js
+++ models/Workout.js
@@ -1,56 +1,76 @@
 // Import Mongoose
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Exercise = require("./Exercise");
+/* const Exercise = require("./Exercise");
+const ExerciseSchema = Exercise.Schema; */
 
 // Define Workout Schema
-const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now()
-    },
-    exercises: [
-/*         {
-            type: {
-                type: String,
-                trim: true,
-                lowercase: true,
-                required: "Exercise Type is Required"
-            },
-            name: {
-                type: String,
-                trim: true,
-                required: "Exercise Name is Required"
-            },
-            duration: {
-                type: Number,
-                required: "Exercise Duration (in minutes) is Required"
-            },
-            weight: {
-                type: Number,
-                required: false
-            },
-            sets: {
-                type: Number,
-                required: false
-            },
-            reps: {
-                type: Number,
-                required: false
-            },
-            distance: {
-                type: Number,
-                required: false
+const WorkoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now()
+        },
+        exercises: [
+            // putting exercise schema directly inside
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    lowercase: true,
+                    required: "Exercise Type is Required"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Exercise Name is Required"
+                },
+                duration: {
+                    type: Number,
+                    required: "Exercise Duration (in minutes) is Required"
+                },
+                weight: {
+                    type: Number,
+                    required: false
+                },
+                sets: {
+                    type: Number,
+                    required: false
+                },
+                reps: {
+                    type: Number,
+                    required: false
+                },
+                distance: {
+                    type: Number,
+                    required: false
+                }
             }
-        } */
-/*         {
-            type: Schema.Types.ObjectId,
-            ref: "Exercise"
-        } */
-        { type: Exercise.Schema }
-    ]
-});
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+// Virtual calculating total duration
+WorkoutSchema
+    .virtual("totalDuration")
+    .get(function() {
+        return this.exercises.reduce((totalDur, exercise) => {
+            return totalDur + exercise.duration
+        }, 0);
+    });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
+
+// had issue getting it to work with this syntax
+/*         {
+                type: Schema.Types.ObjectId,
+                ref: "Exercise"
+            } */
+/*             { type: ExerciseSchema } */
